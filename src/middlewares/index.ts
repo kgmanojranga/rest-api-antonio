7c43775de51ba1d6a464fba7f968832210cffe53
Manoj Ranga <manojranga7583@gmail.com>
2023-12-28 12:56:03 +0530

    feat: add isAuthenticated feature

--- src/middlewares/index.ts
+++ src/middlewares/index.ts
@@ -4,7 +4,8 @@ import { getUserBySessionToken } from "./../models/userModel";
 
 export const isAuthenticated = async (
   req: express.Request,
-  res: express.Response
+  res: express.Response,
+  next: express.NextFunction
 ) => {
   try {
     const sessionToken = req.cookies["MANOJ-AUTH"];
@@ -17,6 +18,10 @@ export const isAuthenticated = async (
     if (!existingUser) {
       return res.sendStatus(403);
     }
+
+    merge(req, {identity: existingUser})
+
+    return next();
   } catch (error) {
     console.log(error);
     return res.sendStatus(400);
