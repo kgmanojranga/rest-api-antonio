1f437b21323de9fb59862256a70ad5bb0e377828
Manoj Ranga <manojranga7583@gmail.com>
2023-12-28 16:11:56 +0530

    feat: implement update user feature

--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import {deleteUserById, getUsers} from "../models/userModel";
+import {deleteUserById, getUserById, getUsers} from "../models/userModel";
 
 export const getAllUsers = async (
   req: express.Request,
@@ -21,6 +21,30 @@ export const deleteUser = async (req: express.Request, res: express.Response) =>
 
     const deleteUser = await deleteUserById(id);
     return res.json(deleteUser);
+  } catch(error) {
+    console.log(error);
+    return res.sendStatus(400);
+  }
+}
+
+export const updateUser = async (req: express.Request, res: express.Response)=> {
+  try{
+    const { id } = req.params;
+    const { username } = req.body;
+
+    if(!username) {
+      return res.sendStatus(400);
+    }
+
+    const user = await getUserById(id);
+
+    if(user){
+      user.username = username
+      await user.save();
+    }
+
+    return res.status(200).json(user).end();
+
   } catch(error) {
     console.log(error);
     return res.sendStatus(400);
