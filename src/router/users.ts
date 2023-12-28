f83adf7d40cec6bc7a76e690716d6e59787c9697
Manoj Ranga <manojranga7583@gmail.com>
2023-12-28 13:17:02 +0530

    chore: update users.ts route

--- src/router/users.ts
+++ src/router/users.ts
@@ -1,7 +1,8 @@
 import express from "express";
 
-import { getAllUsers } from "./../controllers/users";
+import { getAllUsers } from "../controllers/users";
+import {isAuthenticated} from "../middlewares";
 
 export default (router: express.Router) => {
-  router.route("/").get(getAllUsers);
+  router.route("/").get(isAuthenticated, getAllUsers);
 };
